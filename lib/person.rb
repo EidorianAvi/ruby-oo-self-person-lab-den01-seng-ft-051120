966f3e8f241993a911c0fd951e43052e1a920900
Rene Avila <reneavila1993@gmail.com>
2020-05-19 03:18:22 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,66 @@
 # your code goes here
+class Person
+  
+  attr_accessor :bank_account
+  attr_reader :name, :happiness, :hygiene
+
+  def initialize name 
+    @name = name 
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+  
+  def happiness= happiness
+    if happiness > 10 
+      @happiness = 10 
+    elsif happiness < 0 
+      @happiness = 0 
+    else 
+      @happiness = happiness
+    end
+  end
+  
+  def hygiene= hygiene
+    if hygiene > 10 
+      @hygiene = 10
+    elsif hygiene < 0 
+      @hygiene = 0 
+    else 
+      @hygiene = hygiene
+    end
+  end
+  
+  def clean?
+    @hygiene > 7 ? true : false
+  end 
+  
+  def happy?
+    @happiness > 7 ? true : false
+  end
+  
+  def get_paid salary 
+    @bank_account += salary 
+    p "all about the benjamins"
+  end
+
+  def take_bath
+    @hygiene += 4
+    p "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+  
+  def work_out
+    @happiness += 2 
+    @hygiene -= 3
+    p "♪ another one bites the dust ♫"
+  end 
+  
+  def call_friend 
+  end 
+  
+  def start_conversation
+  end 
+
+
+
+end
